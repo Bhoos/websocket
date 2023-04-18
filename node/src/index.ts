4ccd0e3158a5e877427147342a852060973d4e6a
Anmol Paudel <p2anmol@gmail.com>
2023-04-18 15:49:12 +0545

    Include ClientRequestArgs in websocket constructor for node to allow passing headers

--- node/src/index.ts
+++ node/src/index.ts
@@ -1,8 +1,9 @@
 import WebSocket from 'ws';
 import { ClientOptions, Event, CloseEvent, MessageEvent, ErrorEvent} from 'ws';
+import { ClientRequestArgs } from 'http';
 import { ReliableWS as WS, BufferType } from '@bhoos/websocket-base'
 //@ts-ignore
 global.WebSocket = WebSocket
-export class ReliableWS extends WS<Event, ErrorEvent, CloseEvent, MessageEvent, ClientOptions> {
+export class ReliableWS extends WS<Event, ErrorEvent, CloseEvent, MessageEvent, ClientOptions | ClientRequestArgs> {
 }
 export {BufferType}
